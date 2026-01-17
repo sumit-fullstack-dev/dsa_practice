4a0b14e719492403c8c38cb3cef062573e4795f0
Sumit Kumar <sumitranjanmishra@gmail.com>
2026-01-17 13:45:17 +0530

    Feat: Trangle Pattern drow based on user input

---
+++ Pattern.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Pattern {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the World of pattern\n");
+        System.out.print("Please enter your row: ");
+        int row= input.nextInt();
+        System.out.print("Please enter your column");
+        int column=input.nextInt();
+        for(int i=0;i<row;i++){
+            for(int j=0;j<column-i;j++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
